8e6266a651cf3c9e857549062b579f35e97f5511
RandiBro234 <randizelda30@gmail.com>
2025-02-25 12:35:34 +0700

    Memasukkan file praktikum

---
+++ 1.2 Prak 22_08.py
@@ -0,0 +1,9 @@
+banyak = int(input('Masukan banyak angka: '))
+kosong = []
+def kali():
+    for bilangan in range(banyak):
+        angka = int(input("Masukkan angka: "))
+        hasil_kali = angka*3
+        kosong.append(hasil_kali)
+kali()
+print(kosong)
